712ff55365da811cc5a4e84a0f16ba3f5ad41284
tylervb2 <tylervb2@illinois.edu>
2021-11-02 12:29:20 -0500

    Add Config.java for client options

---
+++ client/src/client/Config.java
@@ -0,0 +1,16 @@
+package client;
+
+/**
+ * Manages the options for the client
+ * @author Tyler Barton
+ */
+public class Config {
+    /**
+     * The game is hosted locally and does not require server-mode setup.
+     */
+    public static final boolean isLocal = true;
+
+    public static final String CLIENT_TITLE = "CS242 Final Project";
+    public static final int CLIENT_WIDTH = 800;
+    public static final int CLIENT_HEIGHT = 526;
+}
