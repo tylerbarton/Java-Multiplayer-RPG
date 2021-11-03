6fe6b2420f86eb72056d18a219c1c6c58a522f8c
tylervb2 <tylervb2@illinois.edu>
2021-11-03 10:35:42 -0500

    Add Config.java to change client options

---
+++ client/src/client/Config.java
@@ -0,0 +1,18 @@
+package src.client;
+
+/**
+ * Houses configurable options applicable to the entire client.
+ */
+public class Config {
+    // Client Window Configuration
+    public static final String CLIENT_TITLE = "CS242 Final Project by Tyler Barton";
+    public static final int CLIENT_WIDTH = 800;
+    public static final int CLIENT_HEIGHT = 526;
+
+    // Graphical Performance
+    public static final int MAX_SPRITES = 200;
+
+    // Debug Options
+    public static final boolean VERBOSE_MODE = true;
+    public static final boolean DRAW_FPS = true;
+}
