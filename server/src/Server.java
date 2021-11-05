ec408e74c7389b3434f84011cd18837bb822c362
tylervb2 <tylervb2@illinois.edu>
2021-11-05 18:26:27 -0500

    Add Apache log4j2 logging functionality and configuration

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,17 +1,43 @@
 package src;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import src.util.logging.LogUtil;
+
 /**
  * Main server controller
  * @Author Tyler Barton
  */
 public class Server implements Runnable {
+    private boolean running = false;
 
+    // Manages logging of all server details
+    public final static Logger LOGGER;
+
+    // Logger configuration
+    static {
+        LogUtil.configure();
+        LOGGER = LogManager.getLogger();
+    }
+
+    public static void main(String[] args){
+        // TODO: Initialize entity information
+        // TODO: deserialize SQL state information
+        Server s = new Server();
+        s.init();
+    }
+
+    /**
+     * Initialize the server instance
+     */
+    private void init(){
+        LOGGER.info("Hello World!");
+    }
 
     /**
      * Method called when new thread is started
      */
     @Override
     public void run() {
-
     }
 }
