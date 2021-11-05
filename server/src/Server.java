90fc86f7a306c3c97ced43a0936d44702e901ad3
tylervb2 <tylervb2@illinois.edu>
2021-11-05 16:35:50 -0500

    Add Server.Java for later implementation

---
+++ server/src/Server.java
@@ -0,0 +1,17 @@
+package src;
+
+/**
+ * Main server controller
+ * @Author Tyler Barton
+ */
+public class Server implements Runnable {
+
+
+    /**
+     * Method called when new thread is started
+     */
+    @Override
+    public void run() {
+
+    }
+}
