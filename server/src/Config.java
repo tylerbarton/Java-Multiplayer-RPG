a8aca92d50a2533787c2ecc5d6984db04448fe55
tylervb2 <tylervb2@illinois.edu>
2021-11-05 16:36:10 -0500

    Add Config.java for server configuration

---
+++ server/src/Config.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ * Contains configurable options applicable to the entire server.
+ */
+public class Config {
+    /**
+     * Tick (or update) rate for the server
+     */
+    public final static int TICK_RATE = 200;
+    public final static int MAX_PLAYERS = 20;
+}
