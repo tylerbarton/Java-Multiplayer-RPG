b47928916ccf749f7415469ea705fba63c1615c0
tylervb2 <tylervb2@illinois.edu>
2021-11-05 20:52:43 -0500

    Describe class layout in TODO comments for incremental implementation

--- server/src/Server.java
+++ server/src/Server.java
@@ -2,16 +2,17 @@ package src;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import src.model.world.World;
 import src.util.logging.LogUtil;
 
 /**
  * Main server controller
  * @Author Tyler Barton
  */
-public class Server implements Runnable {
+public class Server {
     private boolean running = false;
-
-    // Manages logging of all server details
+    private long startTime;
+    private World world;
     public final static Logger LOGGER;
 
     // Logger configuration
@@ -20,24 +21,41 @@ public class Server implements Runnable {
         LOGGER = LogManager.getLogger();
     }
 
+
+    // If we wanted multiple worlds, we could implement runnable in this class
+    // Then use a ConcurrentHashMap
     public static void main(String[] args){
-        // TODO: Initialize entity information
-        // TODO: deserialize SQL state information
         Server s = new Server();
         s.init();
+        s.run();
     }
 
     /**
      * Initialize the server instance
      */
     private void init(){
-        LOGGER.info("Hello World!");
+        // TODO: Initialize entity information
+        // TODO: deserialize SQL state information
+        startTime = System.currentTimeMillis();
+        LOGGER.info("Started server.");
     }
 
     /**
-     * Method called when new thread is started
+     * Runs the server's main loop
+     * @implNote Naming convention is intention as multi-threading support can be added
      */
-    @Override
-    public void run() {
+    private void run(){
+        while(running){
+            // TODO: Process incoming packets
+            // TODO: Game loop + Game Events
+            // TODO: World updating + Movement updating
+            // TODO: Process outgoing packets
+            // TODO: Book keeping
+            try{
+
+            }catch(final Throwable t){
+                LOGGER.catching(t);
+            }
+        }
     }
 }
