7b41813bcf559cc38deed83e0d193348ed31f62a
tylervb2 <tylervb2@illinois.edu>
2021-11-10 10:05:40 -0600

    Update player load queries to fix misnamed fields

--- server/src/db/Queries.java
+++ server/src/db/Queries.java
@@ -8,8 +8,8 @@ import src.Config;
 public class Queries {
     public static final String PREFIX = Config.DATABASE_NAME + '.';
     public static final String save_PlayerData = "INSERT INTO `" + "players` VALUES (?, ?, ?, ?)";
-    public static final String load_PlayerDataByUser = "SELECT `id`, `username`, `xLocation`, `yLocation`"
+    public static final String load_PlayerDataByUser = "SELECT `uuid`, `username`, `xLocation`, `yLocation`"
             + "FROM `players` WHERE `username`=?";
-    public static final String load_PlayerDataById = "SELECT `id`, `username`, `xLocation`, `yLocation`"
-            + "FROM `players` WHERE `id`=?";
+    public static final String load_PlayerDataById = "SELECT `uuid`, `username`, `xLocation`, `yLocation`"
+            + "FROM `players` WHERE `uuid`=?";
 }
