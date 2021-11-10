9c20ee1799d89361e7d9141b0c8d4b9802e68b8f
tylervb2 <tylervb2@illinois.edu>
2021-11-09 22:49:44 -0600

    Add Player.java to act as the main player object

---
+++ server/src/model/entity/players/Player.java
@@ -0,0 +1,26 @@
+package src.model.entity.players;
+
+import io.netty.channel.Channel;
+import src.model.entity.Creature;
+
+/**
+ * Represents a player object in the game world
+ */
+public class Player extends Creature {
+    private Channel channel;
+    public String username;
+
+    /**
+     * Used to process a login attempt on this player
+     */
+    public void Login(Channel channel, String username){
+        this.channel = channel;
+    }
+
+    /**
+     * Used to process a logout attempt or a disconnect of the user.
+     */
+    public void Logout(){
+
+    }
+}
