6ae3426f5c64149c5222414ca7b41f364b39940a
tylervb2 <tylervb2@illinois.edu>
2021-11-10 01:37:52 -0600

    Add database configuration options in Config.java

--- server/src/Config.java
+++ server/src/Config.java
@@ -4,8 +4,15 @@ package src;
  * Contains configurable options applicable to the entire server.
  */
 public class Config {
+    // CONNECTION OPTIONS
     public final static String HOST = "127.0.0.1";
     public final static int PORT = 10242;
+
+    // DATABASE OPTIONS
+    public final static String DATABASE_HOST = "localhost:3306";
+    public final static String DATABASE_NAME = "ServerData";
+    public final static String DATABASE_USER = "admin";
+    public final static String DATABASE_PASS = "password";
 
     /**
      * Tick (or update) rate for the server
