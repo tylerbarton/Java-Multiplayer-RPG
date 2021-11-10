552e45d0284f731c52185c7096c753510788c80d
tylervb2 <tylervb2@illinois.edu>
2021-11-09 22:37:41 -0600

    Add PacketFactory.java to make packet construction easier

---
+++ server/src/net/PacketFactory.java
@@ -0,0 +1,46 @@
+package src.net;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+/**
+ * Similar to StringBuilder, acts as a way to build a packet with appropriate data
+ * @author Tyler Barton
+ */
+public class PacketFactory {
+    // Variables
+    private int opcode;
+    private ByteBuf payload = Unpooled.buffer();
+
+    public PacketFactory(){ this(0); }
+    public PacketFactory(int opcode){this.opcode = opcode;}
+
+    // Accessors
+    public void setOpcode(int opcode){this.opcode = opcode;}
+
+    // Methods
+    /**
+     * Writes an integer to the payload
+     * @param i the integer to write to the payload
+     */
+    public void writeInt(int i){
+        payload.writeInt(i);
+    }
+
+    /**
+     * Writes a string to the payload
+     *
+     * @param string The string to write
+     */
+    public void writeString(String string) {
+        payload.writeBytes(string.getBytes());
+        payload.writeByte((byte) 10);
+    }
+
+    /**
+     * @return The constructed packet
+     */
+    public Packet getPacket(){
+        return new Packet(opcode, payload);
+    }
+}
