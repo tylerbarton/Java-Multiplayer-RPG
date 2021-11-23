3607438e1e90fd65c8ce7609f6a99737e4d730b8
tylervb2 <tylervb2@illinois.edu>
2021-11-22 19:22:32 -0600

    Add packet decoding to the ConnectionDecoder

--- server/src/net/ConnectionDecoder.java
+++ server/src/net/ConnectionDecoder.java
@@ -1,6 +1,8 @@
 package src.net;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.util.Attribute;
@@ -16,13 +18,34 @@ public class ConnectionDecoder extends ByteToMessageDecoder implements Attribute
     /**
      * Decodes the bytes transmitted over the channel
      * @param ctx Channel context
-     * @param byteBuf Bytes
-     * @param list Objects sent in the pipeline to the next item (ConnectionHandler)
-     * @throws Exception
+     * @param byteBuf Bytes buffer to be decoded to a packet
+     * @param out Objects sent out in the pipeline to the next item (ConnectionHandler)
      */
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> list) throws Exception {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> out) {
+        //System.out.println("Starting decoder.");
+        final Channel channel = ctx.channel();
+        if(byteBuf.readableBytes() > 2) {
+            byteBuf.markReaderIndex();
+            int length = byteBuf.readUnsignedShort();   // Length does not include itself
+            int opcode = byteBuf.readByte() & 0xFF;
 
+            try{
+                if(length > 0){
+                    ByteBuf data = Unpooled.buffer(length-1);   // -1 for opcode
+                    byteBuf.readBytes(data, length-1);
+                    out.add(new Packet(opcode, data, length-1));
+                }
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        } else {
+           byteBuf.resetReaderIndex();
+        }
+
+        // Add packet to the out list
+//        Packet packet = new Packet();
+//        out.add(packet);
     }
 
     @Override
