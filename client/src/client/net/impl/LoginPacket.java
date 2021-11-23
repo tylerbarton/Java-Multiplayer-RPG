0411898542bbfb4cae2e242e4bbc8affa9d1e0b1
tylervb2 <tylervb2@illinois.edu>
2021-11-22 21:17:09 -0600

    Fix LoginPacket not writing username to payload

--- client/src/client/net/impl/LoginPacket.java
+++ client/src/client/net/impl/LoginPacket.java
@@ -8,10 +8,8 @@ import java.net.Socket;
  * Used to log into the server
  */
 public class LoginPacket extends PacketType {
-    String username;
-
     public LoginPacket(Socket socket, String username){
         super(Opcode.Out.LOGIN.getOpcode(), socket);
-        this.username = username;
+        buffer.writeString(username);
     }
 }
